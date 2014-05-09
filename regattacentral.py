2cebcadb8f21cac59896e0968d225279e8936ffa
Jamie Magee <jamie.magee@gmail.com>
2014-05-09 15:24:03 +0100

    Check other regions for races

--- regattacentral.py
+++ regattacentral.py
@@ -9,21 +9,22 @@ from configparser import ConfigParser
 
 
 def parse_calendar(webpage):
+    global dates, events, web, locations, locationsweb
     tree = html.fromstring(webpage)
-    dates = [date.replace(' \n      ', '') for date in tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[2]/text()')]
-    events = tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[4]/a/text()')
-    web = tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[4]/a/@href')
-    locations = tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[10]/a/text()')
-    locationsweb = tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[10]/a/@href')
-    return dates, events, web, locations, locationsweb
+    dates.append([datetime.strptime(date.replace(' \n      ', ''), '%A%m/%d/%y') for date in tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[2]/text()')])
+    events.append(tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[4]/a/text()'))
+    web.append(tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[4]/a/@href'))
+    locations.append(tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[10]/a/text()'))
+    locationsweb.append(tree.xpath('//*[@id="tableResults"]/tbody/tr[*]/td[10]/a/@href'))
 
 
 def generate_table(dates, events, web, locations, locationsweb):
     out = '| **Race** | **Date** | **Venue** |\n|-|-|-|\n'
     for i in range(len(dates)):
-        if (datetime.strptime(dates[i], '%A%m/%d/%y') - datetime.now()).days < 7:
-            out += '| [' + events[i] + '](https://www.regattacentral.com' + web[i] + ') | ' + datetime.strptime(dates[i], '%A%m/%d/%y').strftime('%d %B') + ' | [' + locations[i] + '](https://www.regattacentral.com' + locationsweb[i] + ') |\n'
-        else: return out
+        if (dates[i] - datetime.now()).days < 7:
+            out += '| [' + events[i] + '](https://www.regattacentral.com' + web[i] + ') | ' + dates[i].strftime('%d %B') + ' | [' + locations[i] + '](https://www.regattacentral.com' + locationsweb[i] + ') |\n'
+        else:
+            return out
 
 
 def set_sidebar(out):
@@ -52,12 +53,26 @@ def set_sidebar(out):
     r.clear_authentication()
 
 
+def flatten_list(list_of_lists):
+    return [item for sublist in list_of_lists for item in sublist]
+
+
 url = 'https://www.regattacentral.com/regattas/index.jsp?c='
+countries = ['AU', 'CA', 'DE', 'IT', 'US']
 while True:
-    print('[*] Fetching page...')
-    page = request.urlopen(url).read().decode('utf-8')
-    print('[*] Parsing calendar...')
-    dates, events, web, locations, locationsweb = parse_calendar(page)
+    dates, events, web, locations, locationsweb = [], [], [], [], []
+    for country in countries:
+        print('[*] Fetching page...')
+        page = request.urlopen(url+country).read().decode('utf-8')
+        print('[*] Parsing calendar...')
+        parse_calendar(page)
+
+    dates = sorted(flatten_list(dates))
+    events = [x for (y, x) in sorted(zip(dates, flatten_list(events)))]
+    web = [x for (y, x) in sorted(zip(dates, flatten_list(web)))]
+    locations = [x for (y, x) in sorted(zip(dates, flatten_list(locations)))]
+    locationsweb = [x for (y, x) in sorted(zip(dates, flatten_list(locationsweb)))]
+
     print('[*] Generating table...')
     out = generate_table(dates, events, web, locations, locationsweb)
     print('[*] Updating sidebar...')
